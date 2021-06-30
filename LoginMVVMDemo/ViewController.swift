d96b62ec8dd681894280a4eaca7e17f504183c5a
Akanksha <akankshashiraskar@gmail.com>
2021-06-30 15:10:22 +0530

    Initial Commit

---
+++ LoginMVVMDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginMVVMDemo
+//
+//  Created by `baps on 30/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
